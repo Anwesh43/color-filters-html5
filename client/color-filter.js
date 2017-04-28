d297d8cfebee9bf92cd3a184ad91d0b1794f24bc
Anwesh43 <anweshthecool0@gmail.com>
2017-04-28 15:51:57 +0530

    Adding javascript file

---
+++ client/color-filter.js
@@ -0,0 +1,33 @@
+class ColorFilter {
+    constructor(imId) {
+        const imageElement = document.getElementById(imId)
+        this.image = new Image()
+        this.image.src = imageElement.src
+        this.imageLoaded = false
+        this.image.onload = ()=>{
+            this.imageLoaded = true
+        }
+    }
+    filter(color) {
+        if(this.imageLoaded) {
+            const canvas = document.createElement('canvas')
+            canvas.width = this.image.width
+            canvas.height = this.image.height
+            const context = canvas.getContext('2d')
+            context.clearRect(0,0,canvas.width,canvas.height)
+            context.save()
+            context.drawImage(this.image,0,0)
+            context.restore()
+            context.fillStyle = color
+            context.globalAlpha = 0.5
+            context.fillRect(0,0,canvas.width,canvas.height)
+            const dataUrl = canvas.getDataURL()
+            console.log(dataUrl)
+        }
+        else {
+            setTimeout(()=>{
+                filter(color)
+            },1000)
+        }
+    }
+}

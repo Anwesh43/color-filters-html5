dd2463921ddfeefe25e1ce4a884e330eff37e855
Anwesh43 <anweshthecool0@gmail.com>
2017-04-28 17:20:34 +0530

    ColorFilter for many components

--- client/color-filter.js
+++ client/color-filter.js
@@ -1,35 +1,32 @@
 class ColorFilter {
     constructor(imId) {
-        const imageElement = document.getElementById(imId)
-        this.imageElement = imageElement
-        this.image = new Image()
-        this.image.src = imageElement.src
-        this.imageLoaded = false
-        this.image.onload = ()=>{
-            this.imageLoaded = true
-        }
+        const imageElements = document.querySelectorAll(imId)
+        this.imageElements = imageElements
+        console.log(imageElements.length)
+    }
+    filterSingleImage(image,imageElement,color) {
+      const canvas = document.createElement('canvas')
+      canvas.width = image.width
+      canvas.height = image.height
+      const context = canvas.getContext('2d')
+      context.clearRect(0,0,canvas.width,canvas.height)
+      context.save()
+      context.drawImage(image,0,0)
+      context.restore()
+      context.fillStyle = color
+      context.globalAlpha = 0.5
+      context.fillRect(0,0,canvas.width,canvas.height)
+      const dataUrl = canvas.toDataURL()
+      imageElement.src = dataUrl
+      console.log(dataUrl)
     }
     filter(color) {
-        if(this.imageLoaded) {
-            const canvas = document.createElement('canvas')
-            canvas.width = this.image.width
-            canvas.height = this.image.height
-            const context = canvas.getContext('2d')
-            context.clearRect(0,0,canvas.width,canvas.height)
-            context.save()
-            context.drawImage(this.image,0,0)
-            context.restore()
-            context.fillStyle = color
-            context.globalAlpha = 0.5
-            context.fillRect(0,0,canvas.width,canvas.height)
-            const dataUrl = canvas.toDataURL()
-            this.imageElement.src = dataUrl
-            console.log(dataUrl)
-        }
-        else {
-            setTimeout(()=>{
-                this.filter(color)
-            },1000)
-        }
+        this.imageElements.forEach((imageElement)=>{
+            const image = new Image()
+            image.src = imageElement.src
+            image.onload = ()=>{
+                this.filterSingleImage(image,imageElement,color)
+            }
+        })
     }
 }
